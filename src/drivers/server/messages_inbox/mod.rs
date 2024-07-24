1ebd9d22a02393e2e7970a62db7f4aa8a551b770
Nikita Podvirnyi <krypt0nn@vk.com>
2024-07-24 12:06:08 +0200

    feat: initial rewrite of `StoredQueueMessagesInbox` to support on-drive storage

--- src/drivers/server/messages_inbox/mod.rs
+++ src/drivers/server/messages_inbox/mod.rs
@@ -10,13 +10,26 @@ pub mod stored_queue;
 /// sent by external clients and meant to be read
 /// by local clients.
 pub trait MessagesInbox {
+    type Error: std::error::Error + Send + Sync;
+
     /// Add new message to the inbox.
-    async fn add_message(&self, sender: Sender, receiver: PublicKey, channel: String, message: Message);
+    async fn add_message(
+        &self,
+        sender: Sender,
+        receiver: PublicKey,
+        channel: String,
+        message: Message
+    ) -> Result<(), Self::Error>;
 
     /// Read client's inbox, applying given filters.
     /// 
     /// Return list of read messages and number of remained.
     /// 
     /// This method will remove read messages from the inbox.
-    async fn poll_messages(&self, receiver: PublicKey, channel: String, limit: Option<u64>) -> (Vec<MessageInfo>, u64);
+    async fn poll_messages(
+        &self,
+        receiver: PublicKey,
+        channel: String,
+        limit: Option<u64>
+    ) -> Result<(Vec<MessageInfo>, u64), Self::Error>;
 }
--- src/drivers/server/messages_inbox/stored_queue.rs
+++ src/drivers/server/messages_inbox/stored_queue.rs
@@ -1,42 +1,58 @@
-use std::time::Duration;
+use std::path::PathBuf;
 
-use moka::future::Cache;
+use serde_json::Value as Json;
 
 use crate::time::timestamp;
 
-use crate::crypto::asymmetric::PublicKey;
+use crate::crypto::prelude::*;
 use crate::rest_api::prelude::*;
 
 use super::MessagesInbox;
 
-#[derive(Debug, Clone)]
-pub struct StoredQueueMessagesInbox {
-    pub inbox: Cache<PublicKey, Vec<MessageInfo>>
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+
+    #[error(transparent)]
+    Json(#[from] AsJsonError),
+
+    #[error(transparent)]
+    Serialize(#[from] serde_json::Error)
 }
 
-impl Default for StoredQueueMessagesInbox {
-    #[inline]
-    fn default() -> Self {
-        Self::new(Duration::from_secs(60 * 60 * 24))
-    }
+#[derive(Debug, Clone)]
+pub struct StoredQueueMessagesInbox {
+    /// Path to the messages inbox's folder.
+    pub storage_folder: PathBuf
 }
 
 impl StoredQueueMessagesInbox {
-    pub fn new(ttl: Duration) -> Self {
-        #[cfg(feature = "tracing")]
-        tracing::trace!("Building new StoredQueueMessagesInbox with {} seconds lifetime", ttl.as_secs());
+    pub async fn new(storage_folder: impl Into<PathBuf>) -> std::io::Result<Self> {
+        let storage_folder = storage_folder.into();
 
-        Self {
-            inbox: Cache::builder()
-                .time_to_idle(ttl)
-                .build()
-        }
+        #[cfg(feature = "tracing")]
+        tracing::trace!("Building new StoredQueueMessagesInbox in {:?}", storage_folder);
+
+        tokio::fs::create_dir_all(&storage_folder).await?;
+
+        Ok(Self {
+            storage_folder
+        })
     }
 }
 
 #[async_trait::async_trait]
 impl MessagesInbox for StoredQueueMessagesInbox {
-    async fn add_message(&self, sender: Sender, receiver: PublicKey, channel: String, message: Message) {
+    type Error = Error;
+
+    async fn add_message(
+        &self,
+        sender: Sender,
+        receiver: PublicKey,
+        channel: String,
+        message: Message
+    ) -> Result<(), Self::Error> {
         #[cfg(feature = "tracing")]
         tracing::debug!(
             sender = ?sender,
@@ -45,20 +61,40 @@ impl MessagesInbox for StoredQueueMessagesInbox {
             "Adding new message"
         );
 
-        let mut inbox = self.inbox.get(&receiver).await
-            .unwrap_or_default();
+        let folder = self.storage_folder
+            .join(receiver.to_base64())
+            .join(&channel);
 
-        inbox.push(MessageInfo {
+        tokio::fs::create_dir_all(&folder).await?;
+
+        let mut index = tokio::fs::read(folder.join("index")).await
+            .unwrap_or(vec![]);
+
+        let message_id = safe_random_u64();
+
+        index.extend_from_slice(&message_id.to_be_bytes());
+
+        let message_info = MessageInfo {
             sender,
             channel,
             message,
             received_at: timestamp()
-        });
+        };
 
-        self.inbox.insert(receiver, inbox).await;
+        let message_info = serde_json::to_vec(&message_info.to_json()?)?;
+
+        tokio::fs::write(folder.join("index"), index).await?;
+        tokio::fs::write(folder.join(message_id.to_string()), message_info).await?;
+
+        Ok(())
     }
 
-    async fn poll_messages(&self, receiver: PublicKey, channel: String, limit: Option<u64>) -> (Vec<MessageInfo>, u64) {
+    async fn poll_messages(
+        &self,
+        receiver: PublicKey,
+        channel: String,
+        limit: Option<u64>
+    ) -> Result<(Vec<MessageInfo>, u64), Self::Error> {
         #[cfg(feature = "tracing")]
         tracing::debug!(
             receiver = receiver.to_base64(),
@@ -67,32 +103,52 @@ impl MessagesInbox for StoredQueueMessagesInbox {
             "Polling messages"
         );
 
-        let mut inbox = self.inbox.get(&receiver).await
-            .unwrap_or_default();
+        let folder = self.storage_folder
+            .join(receiver.to_base64())
+            .join(&channel);
 
-        let mut messages = Vec::new();
+        if let Ok(index) = tokio::fs::read(folder.join("index")).await {
+            assert!(index.len() % 8 == 0);
 
-        let mut limit = limit.unwrap_or(inbox.len() as u64);
-        let mut i = 0;
+            let mut bytes = [0; 8];
+            let mut limit = limit.unwrap_or(u64::MAX);
+            let mut shift = 0;
 
-        while inbox.len() > i && limit > 0 {
-            let record = &inbox[i];
+            let mut messages = Vec::new();
 
-            if record.channel == channel {
-                limit -= 1;
+            for message_id in index.chunks(8) {
+                if limit == 0 {
+                    break;
+                }
 
-                messages.push(inbox.remove(i));
+                bytes.copy_from_slice(message_id);
+
+                let message_id = u64::from_be_bytes(bytes);
+                let message_path = folder.join(message_id.to_string());
+
+                if let Ok(message_info) = tokio::fs::read(&message_path).await {
+                    let message_info = serde_json::from_slice::<Json>(&message_info)?;
+
+                    messages.push(MessageInfo::from_json(&message_info)?);
+
+                    limit -= 1;
+
+                    tokio::fs::remove_file(message_path).await?;
+                }
+
+                shift += 8;
             }
 
-            else {
-                i += 1;
-            }
+            let index = &index[shift..];
+
+            tokio::fs::write(folder.join("index"), index).await?;
+
+            return Ok((
+                messages,
+                (index.len() / 8) as u64
+            ));
         }
 
-        let ramined = inbox.len() as u64;
-
-        self.inbox.insert(receiver, inbox).await;
-
-        (messages, ramined)
+        Ok((vec![], 0))
     }
 }
